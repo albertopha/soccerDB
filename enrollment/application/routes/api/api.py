60f82d166590fbd5696e119c9cb26430cdf76806
Albert Oh <albertopha@hotmail.com>
2019-10-20 21:54:58 -0400

    adding api

--- enrollment/application/routes/api/api.py
+++ enrollment/application/routes/api/api.py
@@ -1,6 +1,13 @@
-from flask import Blueprint
+import requests
+from flask import Blueprint, jsonify
+
 
 api_routes = Blueprint('app_routes', __name__)
+
+url = "https://www.scorebat.com/video-api/v1/"
+
+
 @api_routes.route('/')
 def api():
-    return '<h1>API CALLED</h1>'
+    response = requests.get(url)
+    return jsonify(response.json())
