c7ce6f0fb4f2a8e576b61e6f8d1979c95ada3eb6
Albert Oh <albertopha@hotmail.com>
2019-11-02 23:18:19 -0400

    login form with templates

---
+++ application/forms/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember_me = BooleanField("Remember me")
+    submit = SubmitField("Login")
--- application/routes/views/views.py
+++ application/routes/views/views.py
@@ -1,12 +1,22 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, flash, redirect
 from jinja2 import TemplateNotFound
 
+from soccerDB.application.forms.forms import LoginForm
+
 view_routes = Blueprint('view_routes', __name__,
-                 template_folder='templates')
+                        template_folder='templates')
+
+
 @view_routes.route('/', defaults={"page": "home"})
 @view_routes.route('/<page>')
 def show(page):
     try:
+        if page == 'login':
+            form = LoginForm()
+            if form.validate_on_submit():
+                flash("You are successfully logged in!")
+                return redirect("/")
+            return render_template("login.html", title="Login", form=form, login=True)
         return render_template('index.html')
     except TemplateNotFound:
         abort(500)
